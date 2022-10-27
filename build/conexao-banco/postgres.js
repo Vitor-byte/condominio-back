82ad8d34d99d28aa899cae85773e1b26e982eb7d
Vitor-byte <vitor.cost77@gamil.com>
2022-10-27 16:33:08 -0300

    Altera consulta de condominos buildando

--- build/conexao-banco/postgres.js
+++ build/conexao-banco/postgres.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.client = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+dotenv_1.default.config({ path: '../.env' });
 const isProduction = process.env.NODE_ENV === 'production';
 const { Client } = require("pg");
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
