3bedc26070f83efb59cf15d25297df3fdcefc8e5
Vitor-byte <vitor.cost77@gamil.com>
2022-10-30 13:40:13 -0300

    Alterar caso excluir condomino, trantando os erros com o middleware

--- src/casos-uso/condomino/excluir-condomino-caso.ts
+++ src/casos-uso/condomino/excluir-condomino-caso.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from 'express' 
 import {client} from '../../conexao-banco/postgres'
+import { BadRequestError } from '../../helpers/api-erros';
 
 
 export class excluirCondominoCaso{
@@ -8,7 +9,7 @@ export class excluirCondominoCaso{
 
         const verifica_relacao= await client.query('SELECT COUNT(1) FROM chamado WHERE id_condomino=$1',[id]);
         if(verifica_relacao.rows[0].count != 0){
-            return "Não é possivel excluir";
+            return new BadRequestError('Email invalido!');
         }
         const condomino = await client.query('DELETE FROM condomino WHERE id_condomino=$1 RETURNING *',[id]);
         return condomino;
