2e6265b91e25291d90f9711eeb44ca4a998db95a
Vitor-byte <vitor.cost77@gamil.com>
2022-10-31 16:11:59 -0300

    Insere rotas de aviso

--- build/rotas/rotas.js
+++ build/rotas/rotas.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
 const condomino_controlador_1 = require("../controladores/condomino-controlador");
+const aviso_controlador_1 = require("../controladores/aviso-controlador");
 const router = (0, express_1.Router)();
 exports.router = router;
 // Condomino
@@ -10,3 +11,7 @@ router.post("/condomino", new condomino_controlador_1.condominoControlador().inc
 router.delete("/condomino/:id", new condomino_controlador_1.condominoControlador().excluir);
 router.patch("/condomino/:id", new condomino_controlador_1.condominoControlador().alterar);
 router.get("/condominos", new condomino_controlador_1.condominoControlador().consultar);
+router.post("/aviso", new aviso_controlador_1.avisoControlador().incluir);
+router.delete("/aviso/:id", new aviso_controlador_1.avisoControlador().excluir);
+router.patch("/aviso/:id", new aviso_controlador_1.avisoControlador().alterar);
+router.get("/avisos", new aviso_controlador_1.avisoControlador().consultar);
--- src/rotas/rotas.ts
+++ src/rotas/rotas.ts
@@ -1,6 +1,6 @@
 import {Router} from "express";
 import {condominoControlador} from "../controladores/condomino-controlador";
-
+import {avisoControlador} from "../controladores/aviso-controlador";
 const router = Router();
 
 // Condomino
@@ -8,5 +8,10 @@ router.post("/condomino", new condominoControlador().incluir);
 router.delete("/condomino/:id", new condominoControlador().excluir);
 router.patch("/condomino/:id", new condominoControlador().alterar);
 router.get("/condominos", new condominoControlador().consultar);
+
+router.post("/aviso", new avisoControlador().incluir);
+router.delete("/aviso/:id", new avisoControlador().excluir);
+router.patch("/aviso/:id", new avisoControlador().alterar);
+router.get("/avisos", new avisoControlador().consultar);
 
 export{router};
