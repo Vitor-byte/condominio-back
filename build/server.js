45efed6c802e9c7212c87657448bc7483f84862a
Vitor-byte <vitor.cost77@gamil.com>
2022-10-31 16:27:14 -0300

    Altera server para usar middleware de erroo

--- build/server.js
+++ build/server.js
@@ -4,12 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require('dotenv').config();
+require("express-async-errors");
 const express_1 = __importDefault(require("express"));
+const error_1 = require("./middlewares/error");
 const rotas_1 = require("./rotas/rotas");
 var cors = require('cors');
 const app = (0, express_1.default)();
 app.use(cors());
 app.use(express_1.default.json());
 app.use(rotas_1.router);
+app.use(error_1.errorMiddleware);
 app.listen(process.env.PORT || 6000, () => console.log("Server rodando"));
 console.log(process.env.DATABASE_URL);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-require('dotenv').config()
+require('dotenv').config();
+import 'express-async-errors';
 import express from "express";
+import { errorMiddleware } from './middlewares/error';
 import {router} from "./rotas/rotas";
 
 var cors = require('cors')
@@ -7,6 +9,6 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(router);
-
+app.use(errorMiddleware);
 app.listen(process.env.PORT || 6000, () => console.log("Server rodando"));
 console.log(process.env.DATABASE_URL);
