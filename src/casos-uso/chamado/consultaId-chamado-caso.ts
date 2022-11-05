dc4493a4ef2cae5708442ec7b7093032e4439ed5
Vitor-byte <vitor.cost77@gamil.com>
2022-11-05 18:33:33 -0300

    Altera caso consultar chamado para ser por id de usuario

---
+++ src/casos-uso/chamado/consultaId-chamado-caso.ts
@@ -0,0 +1,15 @@
+import {Request, Response} from 'express' 
+import {client} from '../../conexao-banco/postgres'
+import { BadRequestError } from '../../helpers/api-erros';
+
+export class consultaIdChamadoCaso{
+    async handle(reqParams: any){
+        const {id} = reqParams;
+
+        const chamados = await client.query('SELECT * FROM chamado WHERE id=$1 RETURNING *',
+        [id]);
+
+        return chamados.rows;
+    }
+}
+
