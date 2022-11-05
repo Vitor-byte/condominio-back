0fc34b1587db87c1373c68873d099211b83b6b93
Vitor-byte <vitor.cost77@gamil.com>
2022-11-05 20:01:20 -0300

    Corrige inclusão de chamado

--- src/casos-uso/chamado/incluir-chamado-caso.ts
+++ src/casos-uso/chamado/incluir-chamado-caso.ts
@@ -1,9 +1,8 @@
 import {client} from '../../conexao-banco/postgres';
 export class incluirChamadoCaso{
-    async handle(reqParams:any, reqbody:any){  
-        const {id_usuario} = reqParams;
-        const {titulo, descricao} = reqbody;
-           
+    async handle(reqbody:any){  
+        const {titulo, descricao, id_usuario} = reqbody;
+        
         const chamado = await client.query('INSERT INTO chamado(titulo, descricao, situacao, id_usuario) VALUES ($1, $2, $3, $4) RETURNING *',
         [titulo, descricao, "Aberto", id_usuario]);
 
