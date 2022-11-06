ceb10ddf71c7ad1acffb0dd57b81e62ed566fe6c
Vitor-byte <vitor.cost77@gamil.com>
2022-11-05 23:32:53 -0300

    Insere caso finalizar chamado

---
+++ build/casos-uso/chamado/finalizar-chamado-caso.js
@@ -0,0 +1,32 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.finalizarChamadoCaso = void 0;
+const postgres_1 = require("../../conexao-banco/postgres");
+const api_erros_1 = require("../../helpers/api-erros");
+class finalizarChamadoCaso {
+    handle(reqParams) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { id } = reqParams;
+            const chamadoExiste = yield postgres_1.client.query('SELECT COUNT(1) FROM chamado WHERE id_chamado=$1', [id]);
+            if (chamadoExiste.rows[0].count == 0) {
+                throw new api_erros_1.BadRequestError('Chamado não existe!');
+            }
+            const chamadoSituacao = yield postgres_1.client.query('SELECT COUNT(1) FROM chamado WHERE id_chamado=$1 AND situacao=$2', [id, "Finalizado"]);
+            if (chamadoSituacao.rows[0].count > 0) {
+                throw new api_erros_1.BadRequestError('Não é possivel cancelar o chamado!');
+            }
+            const chamado = yield postgres_1.client.query('UPDATE chamado SET situacao=$2 WHERE id_chamado=$1 RETURNING *', [id, "Cancelado"]);
+            return chamado.rows;
+        });
+    }
+}
+exports.finalizarChamadoCaso = finalizarChamadoCaso;
