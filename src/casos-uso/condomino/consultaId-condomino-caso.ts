e75588dba9c2a32b787fbc593395de3ae6108ac3
Vitor-byte <vitor.cost77@gamil.com>
2022-11-08 00:45:10 -0300

    Inclui consulta de condomino por Id

---
+++ src/casos-uso/condomino/consultaId-condomino-caso.ts
@@ -0,0 +1,19 @@
+import { Console } from 'console';
+import {Request, Response} from 'express' 
+import {client} from '../../conexao-banco/postgres';
+import { BadRequestError } from '../../helpers/api-erros';
+
+
+export class consultaIdCondominoCaso{
+    async handle(reqParams: any){
+        const {id} = reqParams;
+
+        const usuario= await client.query('SELECT COUNT(1) FROM usuario WHERE id_usuario=$1',[id]);
+        
+        if(usuario.rowCount == 0){
+            throw new BadRequestError("Usuario não existe!");
+        }
+
+        return usuario.rows;
+    }
+}
