944c9e792860094cc56f23a4068196f6f779172c
Vitor-byte <vitor.cost77@gamil.com>
2022-11-27 21:19:19 -0300

    Corrige caso

---
+++ build/casos-uso/chamado/consultar-chamado-andamento-caso.js
@@ -0,0 +1,22 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.consultarChamadoEmAndamentoCaso = void 0;
+const postgres_1 = require("../../conexao-banco/postgres");
+class consultarChamadoEmAndamentoCaso {
+    handle() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const chamados = yield postgres_1.client.query('SELECT * FROM chamado WHERE situacao=$1', ["Em andamento"]);
+            return chamados.rows;
+        });
+    }
+}
+exports.consultarChamadoEmAndamentoCaso = consultarChamadoEmAndamentoCaso;
