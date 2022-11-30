3225bf7cd79ab6a8ea43740f9e6e06cde6a7393a
Vitor-byte <vitor.cost77@gamil.com>
2022-11-30 13:36:26 -0300

    corrige caso de reserva

--- build/casos-uso/area/reservar-area-caso.js
+++ build/casos-uso/area/reservar-area-caso.js
@@ -19,11 +19,10 @@ class reservarAreaCaso {
             const dataFormatada = data.split(" ")[0].split("/").reverse().join('-');
             const reservaExiste = yield postgres_1.client.query('SELECT COUNt(1) FROM reserva_area_comum WHERE id_usuario=$1 AND situacao=$2', [id_usuario, "Reservada"]);
             const situacaoCondomino = yield postgres_1.client.query('SELECT COUNT(1) FROM usuario WHERE id_usuario=$1 AND inadimplente=$2', [id_usuario, "Sim"]);
-            if (situacaoCondomino.rowCount > 0) {
+            console.log(situacaoCondomino);
+            console.log(situacaoCondomino.rowCount);
+            if (situacaoCondomino.rows[0] > 0) {
                 throw new api_erros_1.BadRequestError("Regularize sua situção com o condomínio!");
-            }
-            if (reservaExiste.rowCount > 0) {
-                throw new api_erros_1.BadRequestError("Não é possivel fazer outra reserva!");
             }
             const reserva = yield postgres_1.client.query('INSERT INTO reserva_area_comum(id_usuario, id_area_comum, data, horario_inicial, horario_final, situacao ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [id_usuario, id_area_comum, dataFormatada, horario_inicial, horario_final, "Reservada"]);
             return reserva.rows;
--- src/casos-uso/area/reservar-area-caso.ts
+++ src/casos-uso/area/reservar-area-caso.ts
@@ -8,15 +8,14 @@ export class reservarAreaCaso{
   
         const reservaExiste = await client.query('SELECT COUNt(1) FROM reserva_area_comum WHERE id_usuario=$1 AND situacao=$2',[id_usuario,"Reservada"]);
         const situacaoCondomino = await client.query('SELECT COUNT(1) FROM usuario WHERE id_usuario=$1 AND inadimplente=$2',[id_usuario, "Sim"]);
+        console.log(situacaoCondomino)
 
-        if(situacaoCondomino.rowCount > 0){
+        console.log(situacaoCondomino.rowCount)
+        if(situacaoCondomino.rows[0]> 0){
             throw new BadRequestError("Regularize sua situção com o condomínio!");
          }
 
-        if(reservaExiste.rowCount > 0){
-            throw new BadRequestError("Não é possivel fazer outra reserva!");
-         }
-
+    
         const reserva = await client.query('INSERT INTO reserva_area_comum(id_usuario, id_area_comum, data, horario_inicial, horario_final, situacao ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
         [id_usuario, id_area_comum, dataFormatada, horario_inicial, horario_final, "Reservada"]);
         
