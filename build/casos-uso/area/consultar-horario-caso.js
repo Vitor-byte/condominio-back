171ec5ff8e6fd1eab1b4afd8737ce5e39a15378f
Vitor-byte <vitor.cost77@gamil.com>
2022-11-29 21:59:04 -0300

    Teste

--- build/casos-uso/area/consultar-horario-caso.js
+++ build/casos-uso/area/consultar-horario-caso.js
@@ -41,7 +41,7 @@ class consultarHorarioCaso {
                     }
                 }
             }
-            return { "horarios": '23:00:00-24:00:00' };
+            return horariosDisponiveis;
         });
     }
 }
