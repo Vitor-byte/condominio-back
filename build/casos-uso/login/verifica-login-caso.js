20895e814955c09f45bf980bd0e1fef145c55d8b
Vitor-byte <vitor.cost77@gamil.com>
2022-12-03 12:15:26 -0300

    corrige login

--- build/casos-uso/login/verifica-login-caso.js
+++ build/casos-uso/login/verifica-login-caso.js
@@ -16,6 +16,7 @@ class verificaLoginCaso {
     handle(reqBody) {
         return __awaiter(this, void 0, void 0, function* () {
             const { email, senha } = reqBody;
+            console.log(email, senha);
             const usuarioExiste = yield postgres_1.client.query('SELECT * FROM usuario WHERE email=$1 AND senha=$2', [email, senha]);
             if (usuarioExiste.rows[0] == 0) {
                 throw new api_erros_1.BadRequestError('Email ou senha incorretos!');
--- src/casos-uso/login/verifica-login-caso.ts
+++ src/casos-uso/login/verifica-login-caso.ts
@@ -3,7 +3,7 @@ import { BadRequestError } from '../../helpers/api-erros';
 export class  verificaLoginCaso{
     async handle(reqBody:any){  
         const {email, senha} = reqBody;
-        
+        console.log(email, senha)
         const usuarioExiste = await client.query('SELECT * FROM usuario WHERE email=$1 AND senha=$2',[email, senha]);
 
         if(usuarioExiste.rows[0] == 0){
