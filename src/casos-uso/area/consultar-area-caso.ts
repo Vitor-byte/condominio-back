59d4e4c75d3a441dba9126a5f7b1bd465420dd45
Vitor-byte <vitor.cost77@gamil.com>
2022-12-03 10:13:39 -0300

    altera consulta area

--- src/casos-uso/area/consultar-area-caso.ts
+++ src/casos-uso/area/consultar-area-caso.ts
@@ -4,7 +4,7 @@ import {client} from '../../conexao-banco/postgres';
 
 export class consultarAreaCaso{
     async handle(){
-        const areas = await client.query('SELECT * FROM area_comum WHERE situacao=$1 ORDER BY id_area_comum DESC');
+        const areas = await client.query('SELECT * FROM area_comum ORDER BY id_area_comum ASC');
         
         return areas.rows;
     }
