f2ce1a1ea11a90aaa29af629c28bd36c38e2dd0b
Vitor-byte <vitor.cost77@gamil.com>
2022-12-03 22:02:34 -0300

    Configura enquete

--- src/casos-uso/enquete/finalizar-enquete-caso.ts
+++ src/casos-uso/enquete/finalizar-enquete-caso.ts
@@ -5,9 +5,8 @@ export class finalizarEnqueteCaso{
         const {id} = reqParams;
 
         const finalizar = await client.query('UPDATE enquete SET situacao=$2 WHERE id_enquete=$1 RETURNING *',[id, "Finalizar"]);
-        const enqueteResultado = await client.query('SELECT count(id_opcao), id_opcao FROM voto_enquete WHERE id_enquete=$1 group by id_opcao',[id]);
 
-        return enqueteResultado.rows;
+        return finalizar.rows;
     }
 }
 
--- src/casos-uso/enquete/resultado-enquete-caso.ts
+++ src/casos-uso/enquete/resultado-enquete-caso.ts
@@ -4,8 +4,7 @@ export class resultadoEnqueteCaso{
     async handle(reqParams:any){      
         const {id} = reqParams;
 
-        const finalizar = await client.query('UPDATE enquete SET situacao=$2 WHERE id_enquete=$1 RETURNING *',[id, "Fechada"]);
-        const enqueteResultado = await client.query('SELECT count(id_opcao), id_opcao FROM voto_enquete WHERE id_enquete=$1 group by id_opcao',[id]);
+        const enqueteResultado = await client.query('select count(voto_enquete.id_opcao),opcoes_enquete.opcao from opcoes_enquete left JOIN voto_enquete ON opcoes_enquete.id_opcao = voto_enquete.id_opcao WHERE voto_enquete.id_enquete=$1 group by opcoes_enquete.id_opcao',[id]);
 
         return enqueteResultado.rows;
     }
