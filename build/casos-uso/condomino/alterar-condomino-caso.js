37ad4f9a0b5e5faffa6346e7ced283084ba45e44
Vitor-byte <vitor.cost77@gamil.com>
2022-12-11 13:50:55 -0300

    Frontend14

--- build/casos-uso/condomino/alterar-condomino-caso.js
+++ build/casos-uso/condomino/alterar-condomino-caso.js
@@ -16,7 +16,7 @@ class alterarCondominoCaso {
     handle(reqParams, reqbody) {
         return __awaiter(this, void 0, void 0, function* () {
             const { id } = reqParams;
-            const { rg, nome, bloco, inadimplente, situacao, unidade, senha } = reqbody;
+            const { rg, nome, bloco, email, inadimplente, situacao, unidade, senha } = reqbody;
             const usuarioExiste = yield postgres_1.client.query('SELECT COUNT(1) FROM usuario WHERE id_usuario=$1', [id]);
             if (usuarioExiste.rows[0].count == 0) {
                 throw new api_erros_1.BadRequestError("Usuario não existe!");
@@ -25,7 +25,7 @@ class alterarCondominoCaso {
             if (rgExiste.rows[0].count > 0) {
                 throw new api_erros_1.BadRequestError('RG inválido!');
             }
-            const usuario = yield postgres_1.client.query('UPDATE usuario SET rg=$2, nome=$3, inadimplente=$4, situacao=$5, bloco=$6, unidade=$7,senha=$8 WHERE id_usuario=$1 RETURNING *', [id, rg, nome, inadimplente, situacao, bloco, unidade, senha]);
+            const usuario = yield postgres_1.client.query('UPDATE usuario SET rg=$2, nome=$3, inadimplente=$4, situacao=$5, bloco=$6, unidade=$7,senha=$8 email=$9 WHERE id_usuario=$1 RETURNING *', [id, rg, nome, inadimplente, situacao, bloco, unidade, senha, email]);
             return usuario.rows;
         });
     }
--- src/casos-uso/condomino/alterar-condomino-caso.ts
+++ src/casos-uso/condomino/alterar-condomino-caso.ts
@@ -4,7 +4,7 @@ import { BadRequestError } from '../../helpers/api-erros';
 export class alterarCondominoCaso{
     async handle(reqParams: any, reqbody: any){
         const {id} = reqParams;
-        const {rg, nome, bloco, inadimplente, situacao, unidade, senha} = reqbody;
+        const {rg, nome, bloco, email, inadimplente, situacao, unidade, senha} = reqbody;
 
         
         const usuarioExiste = await client.query('SELECT COUNT(1) FROM usuario WHERE id_usuario=$1',[id]);
@@ -19,8 +19,8 @@ export class alterarCondominoCaso{
             throw new BadRequestError('RG inválido!');
         }
 
-        const usuario = await client.query('UPDATE usuario SET rg=$2, nome=$3, inadimplente=$4, situacao=$5, bloco=$6, unidade=$7,senha=$8 WHERE id_usuario=$1 RETURNING *',
-        [id, rg, nome, inadimplente, situacao, bloco, unidade, senha]);
+        const usuario = await client.query('UPDATE usuario SET rg=$2, nome=$3, inadimplente=$4, situacao=$5, bloco=$6, unidade=$7,senha=$8 email=$9 WHERE id_usuario=$1 RETURNING *',
+        [id, rg, nome, inadimplente, situacao, bloco, unidade, senha, email]);
 
         return usuario.rows;
     }
